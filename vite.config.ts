a5c2e1e49f716176c3880561f5fc4d6d06921571
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2024-07-19 00:23:38 +0900

    깃페이지 배포

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cyphers-supporter-clone/", //GitHub 저장소 이름으로 변경
   server: {
     proxy: {
       "/api": {
